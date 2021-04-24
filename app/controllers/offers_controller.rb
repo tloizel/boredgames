25f80e21918e9447d855cfbd8d2c227e0fbd3f50
Fabien Roques <contact.fabien.roques@gmail.com>
2021-04-24 16:42:33 +0200

    offer new and create first version

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,26 @@
 class OffersController < ApplicationController
+
+  def new
+    @offer = Offer.new
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @current_user = User.find_by(id: session[:user_id])
+    @offer.user_id = @current_user
+    @offer.active = true
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:game_name, :description, :game_condition, :price, :location, :delivery_type, :language)  
+  end
+
 end
