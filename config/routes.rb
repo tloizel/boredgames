d3fa0717bc5227822d100f5876ca21e5219400f3
Thomas Loizel <thomas.loizel@gmail.com>
2021-04-24 17:41:57 +0200

    created index, and modified seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'offers#index'
+  get "offers", to: "offers#index", as: :offer
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
