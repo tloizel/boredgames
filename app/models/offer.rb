2d40882834f54e31a3d15e1bdd1d9398be6bd250
Fabien Roques <contact.fabien.roques@gmail.com>
2021-04-29 20:07:23 +0200

    Added validations of model Offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,11 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
-  validates :game_condition, inclusion: { in: ["As new", "Good condition", "Used", "Poor"] }
+  validates :game_condition, inclusion: { in: ["As new", "Good condition", "Used", "Poor"] }, presence: true
+  validates :delivery_type, inclusion: { in: ["In person", "Colissimo", "Point Relay"] }, presence: true
+  validates :language, inclusion: { in: ["English", "Mandarin Chinese", "Hindi", "Spanish", "Standard Arabic", "French", "Russian", "Portuguese","German", "Japanese", "Italian"]}, presence: true
+  validates :game_name, presence: { message: " - Please provide the name of the game" }
+  validates :description, presence: { message: " - Please describe the game" }
+  validates :price, presence: { message: " - Please state how much you want for this game" }
+  validates :location, presence: { message: " - Please state where you are located (country, region...)" }
 end
