d32e7f9d840a980ae04b1d25cc9af025c3dee24b
davidbdry <davidbdry@gmail.com>
2021-04-29 20:38:07 +0200

    Home button

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+    @user = User.find(current_user.id)
+    authorize @user
+
+  end
+
+end
