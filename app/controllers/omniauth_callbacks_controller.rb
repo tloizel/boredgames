f0cda0f11030b827388b7c3c4c030ef475eee287
Thomas Loizel <thomas.loizel@gmail.com>
2021-05-02 12:24:22 +0200

    first attempt of google login

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class OmniauthCallbacksController < ApplicationController
+  # replace with your authenticate method
+  skip_before_action :authenticate_user!
+
+  def google_oauth2
+    auth = request.env["omniauth.auth"]
+    user = User.where(provider: auth["provider"], uid: auth["uid"]).first_or_initialize(email: auth["info"]["email"])
+    user.name ||= auth["info"]["name"]
+    user.save!
+
+    user.remember_me = true
+    sign_in(:user, user)
+
+    redirect_to after_sign_in_path_for(user)
+  end
+end
