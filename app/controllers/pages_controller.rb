988e4748f2dff1c66e0f2a15c5b0b7dfdf7c921a
davidbdry <davidbdry@gmail.com>
2021-05-05 14:04:52 +0200

    trying carrousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+    @all_offers = Offer.all.shuffle
+    @offers = @all_offers.first(3)
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -43,3 +43,28 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
   initAutocomplete();
 });
+
+
+// Swiper carrousel js
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
