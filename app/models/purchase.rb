6a5cc949f1fc14aaf855b76ee37bd48eae031bb0
Fabien Roques <contact.fabien.roques@gmail.com>
2021-05-06 20:49:44 +0200

    Corrected a bug : depdendent destroy on purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :offer
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :chosen_delivery, presence: true
 end
